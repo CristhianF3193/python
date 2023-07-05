54b6b82c41cb11cdd620b860a64d41011cd4f2a1
Cristhian Fuentes <cristhianf3193@gmail.com>
2023-07-04 23:26:00 -0500

    fourth commit

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import bootstrap
 
 app = Flask(__name__)
+bootstrap = bootstrap(app)
 
 todos = ['Comprar cafe', 'Enviar solicitud', 'Entregar video al productor']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html')
 
 @app.route('/')
 def index():
